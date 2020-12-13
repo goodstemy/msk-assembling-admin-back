005b091d68b996973e95fea3028effe57f0db260
Ivan <baho.39rus@gmail.com>
2020-12-13 16:02:26 +0300

    Add clients base support

--- src/graphql-types/index.ts
+++ src/graphql-types/index.ts
@@ -1,6 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import Admin from '../models/admin.model';
 import Assembler from '../models/assembler.model';
+import Client from '../models/client.model';
 import {authenticated, Context} from "../db/middlewares";
 
 export const typeDefs = `
@@ -23,15 +24,30 @@ export const typeDefs = `
     isEnabled: Boolean
   }
 
+  input ClientCreate {
+    phone: String!
+    password: String!
+    name: String!
+    isEnabled: Boolean!
+  }
+
+  type Client {
+    phone: String
+    isEnabled: String
+    name: String
+  }
+
 
   type Query { 
     adminLogin(email: String, password: String): Admin
     me: Admin,
-    assemblers: [Assembler] 
+    assemblers: [Assembler],
+    clients: [Client]
   }
   
   type Mutation {
     createAssembler(params: AssemblerCreate): Assembler!
+    createClient(params: ClientCreate): Client!
   }
 `;
 
@@ -81,13 +97,39 @@ const assemblersResolver = (_: any, __: any) => {
   return Assembler.find();
 };
 
+interface ClientCreateParams {
+  params: {
+    password: String,
+    phone: String,
+    isEnabled: String,
+    name: String,
+  }
+}
+
+const createClientResolver = async (_: any, {params}: ClientCreateParams) => {
+  const client = new Client({
+    phone: params.phone,
+    password: params.password,
+    name: params.name,
+    isEnabled: params.isEnabled,
+  });
+
+  return client.save();
+};
+
+const clientsResolver = (_: any, __: any) => {
+  return Client.find();
+};
+
 export const resolvers = {
   Query: {
     adminLogin: adminLoginResolver,
     me: authenticated((root: Object, args: Object, context: Context) => context.admin),
     assemblers: authenticated((root: Object, args: Object, context: Context) => !!context.admin ? assemblersResolver(root, args) : [{}]),
+    clients: authenticated((root: Object, args: Object, context: Context) => !!context.admin ? clientsResolver(root, args) : [{}]),
   },
   Mutation: {
     createAssembler: authenticated((root: Object, args: AssemblerCreateParams, context: Context) => !!context.admin ? createAssemblerResolver(root, args) : {}),
+    createClient: authenticated((root: Object, args: ClientCreateParams, context: Context) => !!context.admin ? createClientResolver(root, args) : {}),
   }
 };
